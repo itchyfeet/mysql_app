cd9e6c5906506b46f416c52974457017feceb5a1
Itchy Feet <itchyfeet@singapore.com>
2011-01-16 14:42:33 +0800

    Itchy Feet Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysqlApp::Application.config.secret_token = 'a48c25d4bcf2d9686bf99c92eddf47790393423c72ac632526127e69c5cad867af62f04069afb4fdac357ed22307b8cbbecfc5357080eeeec26d97df4295668d'
